4a404331c5557fa39f91208059b2103618fb0c54
Alfred Odey <alfredmodey@gmail.com>
2024-07-22 20:55:04 +0100

    Committing the Program file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_plus_VisualStudio22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter phone number: ");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"My name is {name}.\nMy phone number is {phoneNumber}.\n");
+        }
+    }
+}
